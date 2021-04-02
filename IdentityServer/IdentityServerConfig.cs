e7dd20a0faed3375921ccf7e1f7a0916ccb67e8c
Biswas Nandamuri <biswas.bin2@gmail.com>
2021-04-02 23:31:53 +0530

    Add IdentityServer4 and Configuration

---
+++ IdentityServer/IdentityServerConfig.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace IdentityServer
+{
+    public static class IdentityServerConfig
+    {
+        public static List<TestUser> TestUsers
+        {
+            get
+            {
+                var address = new
+                {
+                    street_address = "One Hacker Way",
+                    locality = "Heidelberg",
+                    postal_code = 69118,
+                    country = "Germany"
+                };
+
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "100001",
+                        Username = "alice",
+                        Password = "alice",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "Alice Smith"),
+                            new Claim(JwtClaimTypes.GivenName, "Alice"),
+                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                            new Claim(JwtClaimTypes.Email, "AliceSmith@email.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.Role, "admin"),
+                            new Claim(JwtClaimTypes.WebSite, "https://alice.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address),
+                                IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    }
+                };
+            }
+        }
+
+        public static IEnumerable<Client> Clients => 
+            new []
+            {
+                new Client
+                {
+                    ClientId = "m2m.webapp",
+                    ClientName = "MVC Web App",
+                        
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = {new Secret("SuperSecretPassword".Sha256())},
+                        
+                    AllowedScopes = {"weatherapi.read", "weatherapi.write"}
+                }
+            };
+        
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new []
+            {
+                new ApiScope("weatherapi.read"),
+                new ApiScope("weatherapi.write"),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new []
+            {
+                new ApiResource("weatherapi")
+                {
+                    Scopes = {"weatherapi.read", "weatherapi.write"},
+                    ApiSecrets = {new Secret("ScopeSecret".Sha256())},
+                    UserClaims = {"role"}
+                }
+            };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResource
+                {
+                    Name = "role",
+                    UserClaims = {"role"}
+                }
+            };
+    }
+}
--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +19,16 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configuring Identity Server with In Memory Client, Resources and Scopes
+            // Along with Developer Certificate (Should be removed in production)
+            services.AddIdentityServer()
+                // Clients - These are the various services which use this project to get authentication and authorization
+                .AddInMemoryClients(IdentityServerConfig.Clients)
+                .AddInMemoryIdentityResources(IdentityServerConfig.IdentityResources)
+                .AddInMemoryApiResources(IdentityServerConfig.ApiResources)
+                .AddInMemoryApiScopes(IdentityServerConfig.ApiScopes)
+                .AddTestUsers(IdentityServerConfig.TestUsers)
+                .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +40,7 @@ namespace IdentityServer
             }
 
             app.UseRouting();
+            app.UseIdentityServer();
 
             app.UseEndpoints(endpoints =>
             {
